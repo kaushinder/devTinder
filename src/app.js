4687aed5feccb2d36dd93edffb5caef9d26cb4fe
kaushinder <kaushindersinghraghav@gmail.com>
2026-03-06 16:23:34 +0530

    refactor: restructure user route handlers and add comments for clarity

--- src/app.js
+++ src/app.js
@@ -2,19 +2,31 @@ const express = require("express");
 
 const app = express();
 
+// this will match only GET requests to the /user route
+app.get("/user", (req, res) => {
+  res.send({firstName: "kaushinder", lastName: "singh"});
+});
+
+// this will match only POST requests to the /user route
+app.post("/user", (req, res) => {
+// the data in the database
+  res.send("Data received successfully in database!");
+});
+
+// this will match only PUT requests to the /user route
+app.put("/user", (req, res) => {
+    res.send("Updated successfully");
+});
+
+// this will match only DELETE requests to the /user route
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully");
+});
+
+// this will match all the HTTP methods (GET, POST, PUT, DELETE, etc.) for the /test route
 app.use("/test", (req, res) => {
-  res.end("This is a test route.");
+  res.send("Hello from the server");
 });
-
-app.use("/abc", (req, res) => {
-  res.end("This is about section.");
-});
-
-
-app.use("/", (req, res) => {
-  res.end("Hello World from Express!");
-});
-
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
