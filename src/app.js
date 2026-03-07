b91a2ee48d0ecbc3651ea7a800238ae50a4d4dd9
kaushinder <kaushindersinghraghav@gmail.com>
2026-03-08 01:21:30 +0530

    Dynamic signup Api

--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
-
 const User = require("./models/user");
+
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   // creating a new instance of the User model
-  const user = new User({
-    firstName: "yugam",
-    lastName: "yadav",
-    emailId: "yugam@gmail.com",
-    password: "yadav@123",
-  });
+  const user = new User(req.body);
 
-  try{
+  try {
     await user.save();
-  res.send("User created successfully");
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send("Error creating user" + err.message);
   }
-  catch(err){
-    res.status(400).send("Error creating user"+ err.message);
-  }
-
 });
 
 connectDB()
