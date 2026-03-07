817eef37025a570813ae7660dd9503be1de1e8d9
kaushinder <kaushindersinghraghav@gmail.com>
2026-03-08 03:04:07 +0530

    Add delete and update user endpoints

--- src/app.js
+++ src/app.js
@@ -43,6 +43,33 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+// Delete user from DB
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try{
+    //  const user = await User.findByIdAndDelete({_id: userId});
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  }
+  catch(err){
+    res.status(400).send("Error deleting user" + err.message);
+  }
+});
+
+// Update user details in DB
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  console.log("Data to be updated :-", data, );
+  try{
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {returnDocument: "before"});
+    res.send(user);
+  }
+  catch(err){
+    res.status(400).send("Error updating user" + err.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
