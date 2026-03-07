00bf18ce3135a85e65abea20649e85f585d568ce
kaushinder <kaushindersinghraghav@gmail.com>
2026-03-07 12:13:36 +0530

    writing auth middileware

--- src/app.js
+++ src/app.js
@@ -2,23 +2,29 @@ const express = require("express");
 
 const app = express();
 
-// GET /user => it checks all the app.xxx("matching route") functions
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/", (req, res, next) => {
-  // res.send("Handling / route");
-  next();
+// Handle Auth middleware for all GET,POST,PUT,DELETE requests to /admin route
+app.use("/admin", adminAuth);
+// app.use("/user", userAuth);
+
+
+app.post("/user/login", (req, res) => {
+  res.send("User logged in");
 })
 
+app.get("/user/data", userAuth, (req, res) => {
+  res.send("User gets the data");
+});
 
-app.get(
-  "/user",
-  (req, res, next) => {
-    next();
-  },
-  (req, res, next) => {
-    res.send("user route");
-  },
-);
+app.get("/admin/getAllData", (req, res) => {
+  // logic of checking if the request is authorized or not
+  res.send("All data sent");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("User deleted");
+});
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
