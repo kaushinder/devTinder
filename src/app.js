802c51d1a01c5f391b59a271256dd634eda5eea1
kaushinder <kaushindersinghraghav@gmail.com>
2026-03-07 10:49:33 +0530

    Route handlers

--- src/app.js
+++ src/app.js
@@ -2,14 +2,32 @@ const express = require("express");
 
 const app = express();
 
-// + means that the preceding character can occur one or more times //abc, abbc, abbbc, etc.
-app.get("/user/:userId/:name/:password", (req, res) => {
-    // console.log(req.query);
-    console.log(req.params);
-  res.send({firstName: "kaushinder", lastName: "singh"});
-});
-
+app.use(
+  "/user",
+  [(req, res, next) => {
+    // Route handler
+    // res.send("Route Handler 1");
+    console.log("handling the route user1!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user2!!");
+    // res.send("Response 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user3!!");
+    // res.send("Response 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("handling the route user4!!");
+    res.send("Response 4");
+    // next();
+  }
+]);  
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
-});
+}
+); 
