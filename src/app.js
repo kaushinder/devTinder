06b1939208309e73f6617192fbcc16ef8a7671f1
kaushinder <kaushindersinghraghav@gmail.com>
2026-03-08 02:19:25 +0530

    GET Api user by email and all users by feed

--- src/app.js
+++ src/app.js
@@ -17,6 +17,32 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Get user by email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Error fetching user" + err.message);
+  }
+});
+
+// Feed Api - GET/feed all the users from DB
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Error fetching user" + err.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
